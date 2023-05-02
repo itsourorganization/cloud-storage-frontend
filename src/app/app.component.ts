e4d2755cac39a632b932b64698431b3f858c4c82
nnabse <nnabse@gmail.com>
2023-05-02 12:15:34 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cloud-storage-frontend-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cloud-storage-frontend';
+}
